f3e9823f11b40565b96ccd5a92c9df0b1fe983c4
Shashank-gangwar <gangwar22cse@gmail.com>
2024-05-07 00:32:07 +0530

    cors urls added properly-3

--- src/app.js
+++ src/app.js
@@ -4,11 +4,38 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://66391cca98b79f0008df9f46--storied-bavarois-f9ea67.netlify.app"
+  );
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://66391cca98b79f0008df9f46--storied-bavarois-f9ea67.netlify.app/",
+      "https://66391cca98b79f0008df9f46--storied-bavarois-f9ea67.netlify.app",
     ],
     credentials: true,
   })
