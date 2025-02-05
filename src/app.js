132e1a4b017c6dece22ff9caa07d965f820d964f
Shashank-gangwar <gangwar22cse@gmail.com>
2025-02-05 16:29:06 +0530

    CORS Middleware changed, access to all IP

--- src/app.js
+++ src/app.js
@@ -4,37 +4,37 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(function (req, res, next) {
-  const allowedOrigins = [
-    "http://localhost:5173",
-    "https://storied-bavarois-f9ea67.netlify.app",
-  ];
-  const origin = req.headers.origin;
+// app.use(function (req, res, next) {
+//   const allowedOrigins = [
+//     "http://localhost:5173",
+//     "https://storied-bavarois-f9ea67.netlify.app",
+//   ];
+//   const origin = req.headers.origin;
 
-  // Website you wish to allow to connect
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
+//   // Website you wish to allow to connect
+//   if (allowedOrigins.includes(origin)) {
+//     res.setHeader("Access-Control-Allow-Origin", origin);
+//   }
 
-  // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
+//   // Request methods you wish to allow
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+//   );
 
-  // Request headers you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
+//   // Request headers you wish to allow
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "X-Requested-With,content-type"
+//   );
 
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true);
+//   // Set to true if you need the website to include cookies in the requests sent
+//   // to the API (e.g. in case you use sessions)
+//   res.setHeader("Access-Control-Allow-Credentials", true);
 
-  // Pass to next layer of middleware
-  next();
-});
+//   // Pass to next layer of middleware
+//   next();
+// });
 
 app.use(cors());
 
